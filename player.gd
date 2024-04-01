d7e37548b41fd1c08498ad7600b9631b163d43f8
Andrei Raphel Amang <andreiraphel@gmail.com>
2024-04-01 09:26:27 +0800

    added bird rotation

--- player.gd
+++ player.gd
@@ -4,18 +4,23 @@ extends CharacterBody2D
 @export var speed = 300.0
 @export var jump_velocity = -400.0
 
-var gravity = 5
+var gravity = 1000
 
 func _physics_process(delta):
 	$AnimatedSprite2D.play("fly")
+	velocity.y += gravity * delta
 	
 	# Add the gravity.
-	if not is_on_floor():
-		velocity.y += gravity * delta * speed
+	if velocity.y > 0:
+		$AnimatedSprite2D.stop()
+		if rotation_degrees < 90:
+			rotate(0.05)
 	
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept"):
 		velocity.y = jump_velocity
+		if rotation_degrees > -40:
+			rotate(-1.5)
 		
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
